de42f6e71a077578f01e3df79bd2becf1117f6c1
mspuy3 <mspuy3@gmail.com>
2022-02-18 18:12:17 +0800

    repushing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "tasks#today"
+  # root "home#index"
 
   resources :labels
   resources :tasks
   resources :glues
 
   get '/today', to: 'tasks#today'
+  
 
 end
