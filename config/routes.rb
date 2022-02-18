1505219ea0015e3fec0d3ba2bc49015708afa1f8
mspuy3 <mspuy3@gmail.com>
2022-02-18 22:16:50 +0800

    removed favico line

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/today', to: 'tasks#today'
+  
   root "tasks#today"
   # root "home#index"
 
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   resources :tasks
   resources :glues
 
-  get '/today', to: 'tasks#today'
+  
   
 
 end
